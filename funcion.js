c83773eb275cdc5d02c939121bdd3f6948e21a5f
Carlos Ernesto Herrera Garcia <62263127+Karlos512@users.noreply.github.com>
2024-01-04 17:51:36 -0600

    upload project

---
+++ funcion.js
@@ -0,0 +1,28 @@
+function chatra() {
+  (function (d, w, c) {
+    w.ChatraID = "qMZSSxStyBLpkQFY6";
+    var s = d.createElement("script");
+    w[c] =
+      w[c] ||
+      function () {
+        (w[c].q = w[c].q || []).push(arguments);
+      };
+    s.async = true;
+    s.src = "https://call.chatra.io/chatra.js";
+    if (d.head) d.head.appendChild(s);
+  })(document, window, "Chatra");
+}
+
+function Tawkto() {
+  var Tawk_API = Tawk_API || {},
+    Tawk_LoadStart = new Date();
+  (function () {
+    var s1 = document.createElement("script"),
+      s0 = document.getElementsByTagName("script")[0];
+    s1.async = true;
+    s1.src = "https://embed.tawk.to/6596f2340ff6374032bc9140/1hjaq47mj";
+    s1.charset = "UTF-8";
+    s1.setAttribute("crossorigin", "*");
+    s0.parentNode.insertBefore(s1, s0);
+  })();
+}
